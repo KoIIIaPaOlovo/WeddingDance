158bfd2ac3809d0c715355eabfc198b8469a5039
KoIIIaPaOlovo <44922634+KoIIIaPaOlovo@users.noreply.github.com>
2020-11-25 14:49:38 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function(){ 
     
     burgerButton=document.querySelector('.burger-button__lines');
     burgerMenu=document.querySelector('.burger-overlay');
@@ -12,5 +12,50 @@ document.addEventListener('DOMContentLoaded', function(){ // Аналог $(docu
             burgerButton.querySelector('.burger-button__line').classList.add('burger-button__line--active');
         }
     });
+
+
+
+    $('.coaches__slider').on('reinit init afterChange', function(slick,currentSlide,nextSlide){
+        if (currentSlide.options.slidesToScroll % 2 == 0)
+        {
+            $('.coaches__slider-count').children('.coaches__slider-number').text((currentSlide.currentSlide+currentSlide.options.slidesToScroll)/(currentSlide.options.slidesToScroll));
+            $('.coaches__slider-count').children('.coaches__slider-amount').text((currentSlide.slideCount/currentSlide.options.slidesToScroll));
+            
+        }else if(currentSlide.options.slidesToScroll % 2 != 0 && currentSlide.options.slidesToScroll!=1){
+            $('.coaches__slider-count').children('.coaches__slider-number').text((currentSlide.currentSlide+currentSlide.options.slidesToScroll)/(currentSlide.slideCount/currentSlide.options.slidesToScroll));
+            $('.coaches__slider-count').children('.coaches__slider-amount').text((currentSlide.slideCount/currentSlide.options.slidesToScroll));
+        }
+        else
+        {  
+            $('.coaches__slider-count').children('.coaches__slider-number').text(currentSlide.currentSlide+1);
+            $('.coaches__slider-count').children('.coaches__slider-amount').text(currentSlide.slideCount);
+        }
+    });
+
+    
+  $('.coaches__slider').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 425,
+          settings: {
+            arrows: false,
+            dots: true,
+            centerMode: true,
+            centerPadding: '0px',
+            slidesToShow: 1.2
+          }
+        }
+      ]
+  });
     
   });
